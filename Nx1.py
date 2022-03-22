6fe6ca38ee06f7c98ec95e9acbe64ee052630a5d
TALIBAN-188 <97728119+TALIBAN-188@users.noreply.github.com>
2022-03-22 11:24:15 +0500

    Update Nx1.py

--- Nx1.py
+++ Nx1.py
@@ -7,7 +7,7 @@ from time import sleep
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from TL import xsxsxlll
+    from Zahid import xsxsxlll
     print("\n Congratulations! Your device supported!\n")
     time.sleep(3)
     xsxsxlll()
